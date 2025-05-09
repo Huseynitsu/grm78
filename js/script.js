0cbc86ca30b48667675ab8cedc103a323ad13091
Huseyn <huseynquliyev395@gmail.com>
2025-05-09 11:33:12 +0400

    first-commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    var swiper = new Swiper(".mainSwiper", {
+        autoplay: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        loop: true,
+    });
+    
+    Fancybox.bind("#feedback img", {
+      groupAll: true,
+    });
+})
+
+const header = document.querySelector('.header');
+const secondHeader = document.querySelector('.second-header');
+
+window.addEventListener('scroll', () => {
+    // Əgər ekran eni 1024 piksel və ya daha böyükdürsə
+    if (window.innerWidth >= 1024) {
+        if (window.scrollY > 60) {
+            header.classList.add('scrolled');
+            secondHeader.classList.add('show');
+        } else {
+            header.classList.remove('scrolled');
+            secondHeader.classList.remove('show');
+        }
+    } else {
+        // Kiçik ekranlarda hər iki class-ı sil (əgər varsa)
+        header.classList.remove('scrolled');
+        secondHeader.classList.remove('show');
+    }
+});
